d8fd9025ff04b8eae81640647b6d2d2d5e1b5eb0
Mumoide <restevez1993@gmail.com>
2022-07-14 22:30:05 -0400

    3

--- aplicaciones/usuario/models.py
+++ aplicaciones/usuario/models.py
@@ -5,3 +5,7 @@ class Usuario(models.Model):
     codigo=models.CharField(primary_key=True,max_length=6)
     nombre=models.CharField(max_length=50)
     apellido=models.CharField(max_length=50)
+
+    def __str__(self):
+        texto = "{0} {1}"
+        return texto.format(self.nombre, self.apellido)
