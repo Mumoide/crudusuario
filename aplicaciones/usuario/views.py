091f793cbce52aa02f01f6d0a6ccf76bd5ee829d
Mumoide <restevez1993@gmail.com>
2022-07-15 23:40:28 -0400

    5

--- aplicaciones/usuario/views.py
+++ aplicaciones/usuario/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Usuario
 
 # Create your views here.
 def home(request):
     listaUsuarios = Usuario.objects.all()
     return render(request, "gestionUsuarios.html", {"usuarios": listaUsuarios})
+
+def registrarUsuario(request):
+    codigo=request.POST['txtCodigo']
+    nombre=request.POST['txtNombre']
+    apellido=request.POST['txtApellido']
+
+    usuario=Usuario.objects.create(codigo=codigo, nombre=nombre, apellido=apellido)
+    return redirect('/')
