0a449aec179bfe4bf32e3dfadf2ab70c8e62d96b
Mumoide <restevez1993@gmail.com>
2022-07-15 21:35:41 -0400

    4

--- aplicaciones/usuario/views.py
+++ aplicaciones/usuario/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Usuario
 
 # Create your views here.
 def home(request):
-    return render(request, "gestionUsuarios.html")
+    listaUsuarios = Usuario.objects.all()
+    return render(request, "gestionUsuarios.html", {"usuarios": listaUsuarios})
