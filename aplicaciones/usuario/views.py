ee7811e635ccbae7ad3fc71eb086d6a751922416
Mumoide <restevez1993@gmail.com>
2022-07-17 18:50:48 -0400

    6

--- aplicaciones/usuario/views.py
+++ aplicaciones/usuario/views.py
@@ -13,3 +13,25 @@ def registrarUsuario(request):
 
     usuario=Usuario.objects.create(codigo=codigo, nombre=nombre, apellido=apellido)
     return redirect('/')
+
+def eliminarUsuario(request, codigo):
+    usuario = Usuario.objects.get(codigo=codigo)
+    usuario.delete()
+
+    return redirect('/')
+
+def edicionUsuario(request,codigo):
+    usuario = Usuario.objects.get(codigo=codigo)
+    return render(request, "edicionUsuarios.html", {"usuario":usuario})
+
+def editarUsuario(request):
+    codigo=request.POST['txtCodigo']
+    nombre=request.POST['txtNombre']
+    apellido=request.POST['txtApellido']
+
+    usuario = Usuario.objects.get(codigo=codigo)
+    usuario.nombre = nombre
+    usuario.apellido = apellido
+    usuario.save()
+
+    return redirect('/')
